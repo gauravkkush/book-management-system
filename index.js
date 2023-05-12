de07a17542a6115ef150af9220d04da1fbcdc272
Gaurav Kushwaha <35199380+gauravkkush@users.noreply.github.com>
2023-05-12 20:37:47 +0530

    Server Setup done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 8081;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.status(200).json({
+		message: "Server is running",
+	});
+});
+
+app.get("*", (req, res) => {
+	res.status(404).json({
+		message: "Node not found",
+	});
+});
+
+app.listen(PORT, () => {
+	console.log("Server is runnig at port", PORT);
+});
